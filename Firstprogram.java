17e95b0cb286a7c98f850077e4188beb5b82ca13
SUBHAMSHARMA24 <102910907+SUBHAMSHARMA24@users.noreply.github.com>
2023-01-24 14:30:30 +0530

    Create Firstprogram.java

---
+++ Firstprogram.java
@@ -0,0 +1,36 @@
+//public: Acess - the class can be available outside the package
+//javac command - to compile the source code
+//javac <filename.java>
+// when u compile the sourcecode we get the bytecode
+// java <Bytecodefilename>
+    // main is the entry point of a java program
+    // main is method()
+    // main has some signature
+    //void is a keyword- return nothing
+    // static is a keyword- 
+    //its trigger JRE--> JVM--> class loading
+/*String args[] is a command line argument
+String is pre defined class in java
+ * args[]- args is an argument and it name can be anything
+ * []-box syntax- it is a array(argumnts can be N)
+ */
+public class Firstprogram {
+    public static void main(String args[]){
+    //static public void main(String ...arguments){ }
+        /*println()- to print in new line
+        print()-to print in same line
+*/
+System.out.println("Hello World");      //print L alphabet
+System.out.println("*"); 
+System.out.println("*");
+System.out.println("*");
+System.out.print("*");  
+System.out.print("*");
+System.out.print("*");
+System.out.print("*");
+
+
+
+
+    }
+}
