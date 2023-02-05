b4e540815f0f427f97d075d3bb221c45b0788db2
SUBHAMSHARMA24 <102910907+SUBHAMSHARMA24@users.noreply.github.com>
2023-02-05 19:25:10 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,42 @@
+//  Print below pattern
+// 1 
+// 22
+// 333
+// 4444
+// 55555
+
+// public class Pattern_12 {
+//     public static void main(String[] args) {
+//         int a=5;
+//         for(int i=1;i<=a;i++){
+//             System.out.println(" ");
+//             for(int j=1;j<=i;j++){
+//                System.out.print(i);
+//             }
+
+//         }
+//     }
+    
+// }
+
+// Print below pattern
+// $$$$$ 
+// $$$$
+// $$$
+// $$
+// $
+
+// public class Pattern_12 {
+//     public static void main(String[] args) {
+//         int a=5;
+//         for(int i=1;i<=a;i++){
+//             System.out.println(" ");
+//             for(int j=5;j>=i;j--){
+//                System.out.print("$");
+//             }
+
+//         }
+//     }
+    
+// }
+
