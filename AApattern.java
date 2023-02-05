cbcbb60b6f2a42da1a6ace6936dbd20242398e1e
SUBHAMSHARMA24 <102910907+SUBHAMSHARMA24@users.noreply.github.com>
2023-02-05 19:27:34 +0530

    Create AApattern.java

---
+++ AApattern.java
@@ -0,0 +1,18 @@
+// A 
+// BB
+// CCC
+// DDDD
+// EEEEE
+public class Pattern_AB1 {
+    public static void main(String[] args) {
+        int a=69;
+        for(int i=65;i<=a;i++){
+            System.out.println(" ");
+            for(int j=65;j<=i;j++){
+                char b=(char)i;
+                System.out.print(b);
+            }
+        }
+    }
+    
+}
