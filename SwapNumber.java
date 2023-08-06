946f1edd603fe6c233b5477f8a0641ba4bcae254
SUBHAMSHARMA24 <102910907+SUBHAMSHARMA24@users.noreply.github.com>
2023-08-07 00:35:08 +0530

    SwapNumber.java

--- SwapNumber.java
+++ SwapNumber.java
@@ -2,14 +2,14 @@
 public class SwapNumber
 {
     static void swap(int a,int b){
-        System.out.println("Before swap: "+a);
-        System.out.println("Before swap: "+b);
+        System.out.println("Before swap a: "+a);
+        System.out.println("Before swap b: "+b);
         a=a+b;
         b=a-b;
         a=a-b;
         
-        System.out.println("\nAfter swap: "+a);
-        System.out.println("After swap: "+b);
+        System.out.println("\nAfter swap a: "+a);
+        System.out.println("After swap b: "+b);
     }
 	public static void main(String[] args) {
 		int a=3;
